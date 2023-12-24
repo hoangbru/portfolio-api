d10a7db92f67e8e834900174c1778eac3db64e53
chideep <hoangbru07@gmail.com>
2023-12-24 23:10:35 +0700

    init server

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import mongoose from "mongoose";
+import jsonServer from "json-server";
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
 
-import { connectOnlDB } from "./config/connect.js";
-import {
-  categoriesRouter,
-  externalRouter,
-  infoRouter,
-  projectsRouter,
-  socialRouter,
-  technologiesRouter,
-  userRouter,
-} from "./routes/index.js";
+server.use(middlewares);
+server.use(router);
 
-const app = express();
-const port = 8080;
-dotenv.config();
-
-app.use(express.json());
-app.use(cors());
-
-app.use("/api",
-    projectsRouter,
-    categoriesRouter,
-    technologiesRouter,
-    userRouter,
-    infoRouter,
-    socialRouter,
-    externalRouter
-);
-
-mongoose.set("strictQuery", false);
-
-// connect mongodb onl
-connectOnlDB();
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+server.listen(port);
--- schemas/category.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-export const categorySchema = Joi.object({
-  name: Joi.string().required().messages({
-    "string.empty": "Không được bỏ trống tên",
-    "any.required": "Trường 'tên' là bắt buộc",
-  }),
-});
